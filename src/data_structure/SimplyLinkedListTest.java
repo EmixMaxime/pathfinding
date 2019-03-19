f48dbf80ecbfcbfcf0bc5a0cd8285afcb48711e2
mx <emixmaxime@outlook.fr>
2019-03-19 20:51:14 +0100

    add: base of SimplyLinkedList with test class

---
+++ src/data_structure/SimplyLinkedListTest.java
@@ -0,0 +1,12 @@
+package data_structure;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimplyLinkedListTest {
+
+  @org.junit.jupiter.api.BeforeEach
+  void setUp() {}
+
+  @org.junit.jupiter.api.AfterEach
+  void tearDown() {}
+}
