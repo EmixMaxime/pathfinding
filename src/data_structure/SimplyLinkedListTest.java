0fa22ee53fceec0332ffb3e74e8682f525257994
mx <emixmaxime@outlook.fr>
2019-03-19 22:14:36 +0100

    test(SimplyLinkedList): addFirst/addLast with getters

--- src/data_structure/SimplyLinkedListTest.java
+++ src/data_structure/SimplyLinkedListTest.java
@@ -1,12 +1,42 @@
 package data_structure;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Deque;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SimplyLinkedListTest {
 
+  Deque<Integer> list;
+
   @org.junit.jupiter.api.BeforeEach
-  void setUp() {}
+  void setUp() {
+    list = new SimplyLinkedList<Integer>();
+  }
 
   @org.junit.jupiter.api.AfterEach
   void tearDown() {}
+
+  @Test
+  void testAddFirst() {
+    assertNull(list.getFirst());
+
+    list.addFirst(12);
+    assertEquals(12, list.getFirst());
+    assertEquals(12, list.getLast());
+
+    list.addFirst(20);
+    assertEquals(20, list.getFirst());
+    assertEquals(12, list.getLast());
+  }
+
+  @Test
+  void testAddLast() {
+    list.addLast(10);
+    assertEquals(10, list.getLast());
+
+    list.addLast(55);
+    assertEquals(55, list.getLast());
+  }
 }
