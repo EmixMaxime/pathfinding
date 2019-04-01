55913b8e639e4cbaed0979e65b9b8c59f3cc9533
mx <emixmaxime@outlook.fr>
2019-04-01 11:22:38 +0200

    wip

--- src/map/MapMatrix.java
+++ src/map/MapMatrix.java
@@ -3,19 +3,42 @@ package map;
 import traverse.Explorable;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Set;
 
 /** @param <S> the step type */
 public class MapMatrix<S> implements Explorable<S> {
 
   private Object[][] matrix;
+  private final boolean allowsDiagonals;
+
+  /**
+   * Bounds of matrix.
+   */
+  private int xSize;
+  private int ySize;
 
   public MapMatrix(Object[][] matrix) {
-    this.matrix = matrix;
+    this(matrix, false);
+  }
+
+  public MapMatrix(Object[][] matrix, boolean allowsDiagonals) {
+    this.allowsDiagonals = allowsDiagonals;
+  }
+
+  private S goTo() {
+
+    return null;
   }
 
   /** {@inheritDoc} */
   public Set<S> getReachableStepsFrom(S step) {
-    return null;
+    Set<S> reachable = new HashSet<S>();
+
+    if (allowsDiagonals) {
+
+    }
+
+    return reachable;
   }
 }
