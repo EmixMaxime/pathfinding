984b7a28e58405d84edb0a830696e813f04d1c4e
mx <emixmaxime@outlook.fr>
2019-04-01 22:33:20 +0200

    feat(Explorable): param reachable DS + type clean.

--- src/traverse/Explorable.java
+++ src/traverse/Explorable.java
@@ -5,17 +5,21 @@ import java.util.Set;
 /**
  * Make any data structure Explorable.
  *
- * @param <S> the step type.
  */
-public interface Explorable<S> {
+public interface Explorable {
+
+  Set<StepInterface> getReachableStepsFrom(StepInterface step);
 
   /**
    * Returns a set of all steps from the specified step.
    *
    * @param step the step for which the list of steps to be returned.
+   * @param reachable Set of reachable steps.
+   *
    * @return a set of all reachable steps from the specified step.
    * @throws IllegalArgumentException if step is not found in the data structure.
    * @throws NullPointerException if step is <code>null</code>.
    */
-  Set<S> getReachableStepsFrom(S step);
+
+  Set<StepInterface> getReachableStepsFrom(StepInterface step, Set<StepInterface> reachable);
 }
