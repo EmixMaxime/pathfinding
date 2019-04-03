bd9acaa80c664c497247fa77e007cd1d8089cd84
mx <emixmaxime@outlook.fr>
2019-04-03 15:32:05 +0200

    wip feat(Coords2D)

---
+++ src/plan/Coords2D.java
@@ -0,0 +1,38 @@
+package plan;
+
+public class Coords2D {
+  private double x;
+  private double y;
+
+  public Coords2D(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public static double manhattanDistance(Coords2D a, Coords2D b) {
+    return Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
+  }
+
+  public static double euclideanDistance(Coords2D a, Coords2D b) {
+    double yDelta = Math.abs(a.y - b.y);
+    double xDelta = Math.abs(a.x - b.x);
+
+    return Math.sqrt(Math.pow(yDelta, 2) + Math.pow(xDelta, 2));
+  }
+
+  public double getX() {
+    return x;
+  }
+
+  public void setX(double x) {
+    this.x = x;
+  }
+
+  public double getY() {
+    return y;
+  }
+
+  public void setY(double y) {
+    this.y = y;
+  }
+}
---
+++ src/plan/Coords2DTest.java
@@ -0,0 +1,18 @@
+package plan;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Coords2DTest {
+
+  @BeforeEach
+  void setUp() {}
+
+  @Test
+  void manhattanDistance() {}
+
+  @Test
+  void euclideanDistance() {}
+}
