ce06c0c4044717cdca221d18a410ed04eb0d0e7b
Unknown <fraysse.ad@gmail.com>
2019-04-05 21:40:46 +0200

    Correction erreurs test unitaire

--- src/plan/Coords2DTest.java
+++ src/plan/Coords2DTest.java
@@ -1,6 +1,7 @@
 package plan;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -33,15 +34,15 @@ class Coords2DTest {
     Coords2D pointA = new Coords2D(3, 5);
     Coords2D pointB = new Coords2D(8, 9);
 
-    Double ACTUAL = Coords2D.manhattanDistance(pointA, pointB);
+    Double ACTUAL = Coords2D.euclideanDistance(pointA, pointB);
     Double EXPECTED = Math.sqrt(41);
     assertEquals(EXPECTED, ACTUAL);
 
     Coords2D pointC = new Coords2D(7, 8);
     Coords2D pointD = new Coords2D(-9, -9);
 
-    ACTUAL = Coords2D.manhattanDistance(pointC, pointD);
-    EXPECTED =  Math.sqrt(5);
+    ACTUAL = Coords2D.euclideanDistance(pointC, pointD);
+    EXPECTED =  Math.sqrt(545);
     assertEquals(EXPECTED, ACTUAL);
   }
 }
