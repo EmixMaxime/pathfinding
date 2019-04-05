f5676743347d566be8cb97e5d0a1b388a28aa9bb
vienne <geoffreydan@wanadoo.fr>
2019-04-05 22:59:49 +0200

    Fill map
    MapInformation :
    + coordonnée départ
    + coordonnée d'arriver
    
    MapBuilder :
    + lecture coordonées départ et arrivé
    + insertion de données dans la matrice
    
    + MapBuilderTest (test unitaire)

--- src/map/MapInformation.java
+++ src/map/MapInformation.java
@@ -2,6 +2,10 @@ package map;
 
 public class MapInformation {
   private String title;
+  private int startX;
+  private int startY;
+  private int endX;
+  private int endY;
 
   public String getTitle() {
     return title;
@@ -10,4 +14,16 @@ public class MapInformation {
   public void setTitle(String title) {
     this.title = title;
   }
+
+  public int getStartX(){ return startX;}
+  public int getStartY(){ return startY;}
+
+  public void setStartX(int x){ startX = x;}
+  public void setStartY(int y){ startY = y;}
+
+  public int getEndX(){ return endX;}
+  public int getEndY(){ return endY;}
+
+  public void setEndX(int x){ endX = x;}
+  public void setEndY(int y){ endY = y;}
 }
