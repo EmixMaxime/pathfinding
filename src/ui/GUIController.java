687b6cb76309d498adea3e5887fca00632aa07db
mx <emixmaxime@outlook.fr>
2019-04-07 00:08:08 +0200

    tracking a bug

--- src/ui/GUIController.java
+++ src/ui/GUIController.java
@@ -43,79 +43,79 @@ public class GUIController {
         @Override
         protected void succeeded() {
           MapSearch mapSearch = getValue();
-          var mapMatrix = mapSearch.getMap();
-
-          Image road = null;
-          try {
-            road =
-                new Image(
-                    new FileInputStream("resources/imgs/sand_tile.png"),
-                    xTileSize,
-                    yTitleSize,
-                    true,
-                    false);
-          } catch (FileNotFoundException e) {
-            e.printStackTrace();
-          }
-
-          Image obstacle = null;
-          try {
-            obstacle =
-                new Image(
-                    new FileInputStream("resources/imgs/grass_tile_1.png"),
-                    xTileSize,
-                    yTitleSize,
-                    true,
-                    false);
-          } catch (FileNotFoundException e) {
-            e.printStackTrace();
-          }
-
-          Image perso = null;
-          try {
-            perso =
-                new Image(
-                    new FileInputStream("resources/imgs/perso.png"),
-                    xTileSize,
-                    yTitleSize,
-                    true,
-                    false);
-          } catch (FileNotFoundException e) {
-            e.printStackTrace();
-          }
-
-          ImageView win = null;
-          try {
-            win =
-                new ImageView(
-                    new Image(
-                        new FileInputStream("resources/imgs/gem.png"),
-                        xTileSize,
-                        yTitleSize,
-                        true,
-                        false));
-          } catch (FileNotFoundException e) {
-            e.printStackTrace();
-          }
-
-          // Add things into the grid. Obstacle or road!
-          for (int y = 0; y < mapMatrix.getYSize(); y++) {
-            for (int x = 0; x < mapMatrix.getXSize(); x++) {
-              if (mapMatrix.isWalkable(x, y) != null) {
-                ImageView img = new ImageView(road);
-                maze.getChildren().add(img);
-              } else {
-                ImageView img = new ImageView(obstacle);
-                maze.getChildren().add(img);
-              }
-            }
-          }
+//          var mapMatrix = mapSearch.getMap();
+//
+//          Image road = null;
+//          try {
+//            road =
+//                new Image(
+//                    new FileInputStream("resources/imgs/sand_tile.png"),
+//                    xTileSize,
+//                    yTitleSize,
+//                    true,
+//                    false);
+//          } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//          }
+//
+//          Image obstacle = null;
+//          try {
+//            obstacle =
+//                new Image(
+//                    new FileInputStream("resources/imgs/grass_tile_1.png"),
+//                    xTileSize,
+//                    yTitleSize,
+//                    true,
+//                    false);
+//          } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//          }
+//
+//          Image perso = null;
+//          try {
+//            perso =
+//                new Image(
+//                    new FileInputStream("resources/imgs/perso.png"),
+//                    xTileSize,
+//                    yTitleSize,
+//                    true,
+//                    false);
+//          } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//          }
+//
+//          ImageView win = null;
+//          try {
+//            win =
+//                new ImageView(
+//                    new Image(
+//                        new FileInputStream("resources/imgs/gem.png"),
+//                        xTileSize,
+//                        yTitleSize,
+//                        true,
+//                        false));
+//          } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//          }
+//
+//          // Add things into the grid. Obstacle or road!
+//          for (int y = 0; y < mapMatrix.getYSize(); y++) {
+//            for (int x = 0; x < mapMatrix.getXSize(); x++) {
+//              if (mapMatrix.isWalkable(x, y) != null) {
+//                ImageView img = new ImageView(road);
+//                maze.getChildren().add(img);
+//              } else {
+//                ImageView img = new ImageView(obstacle);
+//                maze.getChildren().add(img);
+//              }
+//            }
+//          }
 
           var start = mapSearch.getStart();
           var end = mapSearch.getGoal();
 
-          maze.getChildren().set(computeIndexFromXY(start), new ImageView(perso));
-          maze.getChildren().set(computeIndexFromXY(end), win);
+//          maze.getChildren().set(computeIndexFromXY(start), new ImageView(perso));
+//          maze.getChildren().set(computeIndexFromXY(end), win);
 
           var map = mapSearch.getMap();
           var s = map.getElement(start);
@@ -128,8 +128,8 @@ public class GUIController {
 
           var list = it.path();
           for (var item : list) {
-            System.out.println("x,y of path " + item.getX() + " " + item.getY());
-            maze.getChildren().set(computeIndexFromXY(item.getCoords()), new ImageView(perso));
+//            System.out.println("x,y of path " + item.getX() + " " + item.getY());
+//            maze.getChildren().set(computeIndexFromXY(item.getCoords()), new ImageView(perso));
           }
         }
 
@@ -138,14 +138,14 @@ public class GUIController {
           System.out.println("call in thread for map " + mapName);
           MapSearch mapSearch = new MapBuilder("resources\\map3.txt").getMapSearch();
 
-          maze.setPrefColumns(mapSearch.getMap().getXSize());
-          maze.setPrefRows(mapSearch.getMap().getYSize());
-
-          int xSize = mapSearch.getMap().getXSize();
-          int ySize = mapSearch.getMap().getYSize();
-
-          xTileSize = (GUIMain.WIDTH * 1.0 / xSize);
-          yTitleSize = (GUIMain.HEIGHT * 1.0 / ySize);
+//          maze.setPrefColumns(mapSearch.getMap().getXSize());
+//          maze.setPrefRows(mapSearch.getMap().getYSize());
+//
+//          int xSize = mapSearch.getMap().getXSize();
+//          int ySize = mapSearch.getMap().getYSize();
+//
+//          xTileSize = (GUIMain.WIDTH * 1.0 / xSize);
+//          yTitleSize = (GUIMain.HEIGHT * 1.0 / ySize);
 
           return mapSearch;
         }
