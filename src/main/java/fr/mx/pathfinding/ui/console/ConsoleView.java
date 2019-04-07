cdd515f29994a1ac7206b4804f9dbc0f5c135525
vienne <geoffreydan@wanadoo.fr>
2019-04-07 20:02:16 +0200

    Console
    + Clavier.java
    + menu
    + chose map and algorithm

--- src/main/java/fr/mx/pathfinding/ui/console/ConsoleView.java
+++ src/main/java/fr/mx/pathfinding/ui/console/ConsoleView.java
@@ -3,15 +3,26 @@ package fr.mx.pathfinding.ui.console;
 import fr.mx.pathfinding.map.MapBuilder;
 import fr.mx.pathfinding.map.MapSearch;
 import fr.mx.pathfinding.traverse.BreadthFirstIterator;
+import fr.mx.pathfinding.util.Clavier;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
 
 class ConsoleView {
 
   private MapSearch mapSearch;
   private MapBuilder mapBuilder;
+  private String mapName;
+  private String algorithmName;
 
-  ConsoleView(MapBuilder mapBuilder) {
-    this.mapSearch = mapBuilder.getMapSearch();
-    this.mapBuilder = mapBuilder;
+  //ConsoleView(MapBuilder mapBuilder) {
+  ConsoleView() {
+    //this.mapSearch = mapBuilder.getMapSearch();
+    //this.mapBuilder = mapBuilder;
+    mapName = "";
+    algorithmName = "";
   }
 
   void show() {
@@ -46,5 +57,88 @@ class ConsoleView {
     }
 
     System.out.println(mapBuilder.matrixToString());
+  }
+
+  void menu() {
+    System.out.println("a - chose an algorithm\n" +
+      "m - chose a map\n" +
+      "l - launch\n" +
+      "h - display the menu\n" +
+      "q - quit\n" +
+      "> ");
+  }
+
+  void choseAlgorithm() {
+
+    System.out.println("0 - Depth\n" +
+      "1 - Length\n" +
+      "2 - AStar\n" +
+      "3 - Other");
+
+    int chose = -1;
+    do {
+      chose = Clavier.lireInt();
+      if (chose == 0)
+        algorithmName = "Depth";
+      else if (chose == 1)
+        algorithmName = "Length";
+      else if (chose == 2)
+        algorithmName = "AStar";
+      else if (chose == 3)
+        algorithmName = "Other";
+      else
+        chose = -1;
+
+
+    } while (chose == -1);
+  }
+
+  void choseMap() {
+
+    Path rootFolder = Paths.get(System.getProperty("user.dir"), "src/main/resources/fr/mx/pathfinding");
+    File folder = new File(rootFolder.toString());
+    List<String> listMap = new ArrayList<>();
+    for (File file : folder.listFiles()) {
+      if (file.getName().contains(".txt")) {
+        listMap.add(file.getName());
+        System.out.println(listMap.size()-1 + " - " + file.getName());
+      }
+    }
+
+    int chose = -1;
+    do {
+      chose = Clavier.lireInt();
+      if (chose <= listMap.size())
+        mapName = listMap.get(chose);
+      else if (chose < 0)
+        chose = -1;
+
+
+    } while (chose == -1);
+  }
+
+  void run(String chose) {
+
+    // Prevent from user stupidity
+    chose = chose.trim().intern();
+    if (chose == "a" || chose == "A") {
+      choseAlgorithm();
+    } else if (chose == "m" || chose == "M") {
+      choseMap();
+    } else if (chose == "h" || chose == "H") {
+      menu();
+    } else if (chose == "l" || chose == "L") {
+      if (!mapName.isEmpty() && !algorithmName.isEmpty()) {
+        this.mapBuilder = new MapBuilder("/" + mapName);
+        this.mapSearch = mapBuilder.getMapSearch();
+
+        show();
+      } else {
+        System.out.println("please chose an Algorithm and a map first >");
+      }
+    }
+    if(chose != "q") {
+      menu();
+    }
   }
 }
