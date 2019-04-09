2cbe31d6038988a0a1c5cee21e7928a640026864
Epsilol <fraysse.ad@gmail.com>
2019-04-09 23:09:27 +0200

    A star modification
    A star comme je l'ai compris, reste a return le path

--- src/main/java/fr/mx/pathfinding/traverse/astar/Astar.java
+++ src/main/java/fr/mx/pathfinding/traverse/astar/Astar.java
@@ -1,4 +1,4 @@
-package fr.mx.pathfinding.traverse.astar;
+package main.java.fr.mx.pathfinding.traverse.astar;
 
 import fr.mx.maths.ToleranceDoubleComparator;
 import fr.mx.pathfinding.traverse.Explorable;
@@ -25,132 +25,153 @@ import java.util.*;
  * <p>A linked list is also not a good data structure for the closed set, where you need fast
  * "Contains", which is O(n) in a linked list. You should use a HashSet for that.
  */
-public class Astar<S> {
-  class NodeComparator implements Comparator<NodeData<S>> {
+public class Astar<S> implements AStarHeuristic {
+    @Override
+    public double getCostEstimate(Object sourceStep, Object targetStep) {
+        return fr.mx.pathfinding.plan.Coords2D.manhattanDistance((fr.mx.pathfinding.plan.Coords2D) sourceStep, (fr.mx.pathfinding.plan.Coords2D) targetStep);
+    }
+
+    class NodeComparator implements Comparator<NodeData<S>> {
+
+        /**
+         * Compare two Node by the score
+         *
+         * @param nodeFirst
+         * @param nodeSecond
+         * @return
+         */
+        public int compare(NodeData<S> nodeFirst, NodeData<S> nodeSecond) {
+            if (nodeFirst.getfScore() > nodeSecond.getfScore()) return 1;
+            if (nodeSecond.getfScore() > nodeFirst.getfScore()) return -1;
+            return 0;
+        }
+    }
+
+    // Comparator for comparing doubles with tolerance
+    private Comparator<Double> comparator;
+
+    //  protected Set<S> openList;
+    protected Set<S> closedList;
+    //  private AStarHeuristic<S> heuristic;
+
+    public Astar(Explorable<S> explorable, S startStep, S endStep) {
+        //    this.heuristic = heuristic;
+        this.comparator = new ToleranceDoubleComparator();
+
+        // The set of currently discovered nodes that are not evaluated yet.
+        // Initially, only the start node is known.
+        PriorityQueue<NodeData<S>> openSet = new PriorityQueue<>(new NodeComparator());
+        Set<NodeData<S>> closedSet = new HashSet<>();
+
+        //Add in the openSet the startStep
+        openSet.add(new NodeData<S>(startStep, null, getCostEstimate(startStep, endStep), 0));
+
+        // For each node, which node it can most efficiently be reached from.
+        // If a node can be reached from many nodes, cameFrom will eventually contain the
+        // most efficient previous step.
+        //    Map<S, NodeData<S>> cameFrom = new HashMap<>();
+
+        while (!openSet.isEmpty()) {
+            // get the node in openSet having the lowest fScore value
+            // openSet is a sorted map on fScore.
+            NodeData<S> current = openSet.peek();
+
+            if (current.step.equals(endStep)) {
+                System.out.println("done");
+                //return path
+                return;
+            }
+
+            for (S step : explorable.getReachableStepsFrom(startStep)) {
+                if (closedSet.contains(step)) {
+                    //Ignore the neighboor which is already evaluated;
+                    continue;
+                }
+
+                // The distance from start to a neighbor
+                double gScore = current.getgScore() + getCostEstimate(current.step, step);
+
+                // discover a new step
+                if (!openSet.contains(step)) {
+                    openSet.add(
+                            new NodeData<S>(
+                                    step,
+                                    current.fromStep,
+                                    getCostEstimate(step, endStep),
+                                    gScore + 1));
+                }
+            }
+            closedSet.add(current);
+        }
+        System.out.println("Chemin introuvable");
+        return;
+    }
 
     /**
-     * Compare two Node by the score
-     * @param nodeFirst
-     * @param nodeSecond
-     * @return
+     * Little class to encapsulate data about StepInterface that's been traversed.
+     *
+     * @param <S> the step type
      */
-    public int compare(NodeData<S> nodeFirst, NodeData<S> nodeSecond) {
-      if (nodeFirst.getfScore() > nodeSecond.getfScore()) return 1;
-      if (nodeSecond.getfScore() > nodeFirst.getfScore()) return -1;
-      return 0;
-    }
-  }
+    static class NodeData<S> implements TracableStep<S> {
+        private S fromStep;
 
-  // Comparator for comparing doubles with tolerance
-  private Comparator<Double> comparator;
+        private S step;
 
-  //  protected Set<S> openList;
-  //  protected Set<S> closedList;
-  //  private AStarHeuristic<S> heuristic;
-
-  public Astar(Explorable<S> explorable, S startStep, S endStep, AStarHeuristic<S> heuristic) {
-    //    this.heuristic = heuristic;
-    this.comparator = new ToleranceDoubleComparator();
-
-    // The set of currently discovered nodes that are not evaluated yet.
-    // Initially, only the start node is known.
-    PriorityQueue<NodeData<S>> openSet = new PriorityQueue<>(new NodeComparator());
-    Set<NodeData<S>> closedSet = new HashSet<>();
-
-    // For each node, which node it can most efficiently be reached from.
-    // If a node can be reached from many nodes, cameFrom will eventually contain the
-    // most efficient previous step.
-    //    Map<S, NodeData<S>> cameFrom = new HashMap<>();
-
-    while (!openSet.isEmpty()) {
-      // get the node in openSet having the lowest fScore value
-      // openSet is a sorted map on fScore.
-      NodeData<S> current = openSet.peek();
-
-      if (current.step.equals(endStep)) {
-        System.out.println("done");
-        return;
-      }
-
-      for (S step : explorable.getReachableStepsFrom(startStep)) {
-        if (closedSet.contains(step)) {
-          // Ignore the neighbor which is already evaluated.
-          continue;
+        public S getStep() {
+            return step;
         }
 
-        // The distance from start to a neighbor
-        //        double gScore = current.getgScore() + heuristic.getCostEstimate(current.step,
-        // step);
-
-        // discover a new step
-        if (!openSet.contains(step)) {
-          openSet.add(
-              new NodeData<S>(
-                  step,
-                  current.fromStep,
-                  heuristic.getCostEstimate(step, endStep),
-                  current.gScore + 1));
+        public void setStep(S step) {
+            this.step = step;
         }
-      }
 
-      closedSet.add(current);
+        // heuristic of destination
+        final double heuristic;
+
+        // gScore is the distance from the source
+        private double gScore;
+
+        // fScore = gScore + heuristic
+        private double fScore;
+
+        NodeData(S step, S fromStep, double heuristic, double gScore) {
+            this.step = step;
+            this.fromStep = fromStep;
+            this.heuristic = heuristic;
+            this.gScore = gScore;
+
+            this.fScore = gScore + heuristic;
+        }
+
+        //    public boolean equals(NodeData n) {}
+
+        @Override
+        public S getPredecessor() {
+            return fromStep;
+        }
+
+        public double getgScore() {
+            return gScore;
+        }
+
+        public void setgScore(double gScore) {
+            this.gScore = gScore;
+        }
+
+        public double getfScore() {
+            return fScore;
+        }
+
+        public void setfScore(double fScore) {
+            this.fScore = fScore;
+        }
+
+        public S getFromStep() {
+            return fromStep;
+        }
+
+        public void setFromStep(S fromStep) {
+            this.fromStep = fromStep;
+        }
     }
-  }
-
-  /**
-   * Little class to encapsulate data about StepInterface that's been traversed.
-   *
-   * @param <S> the step type
-   */
-  static class NodeData<S> implements TracableStep<S> {
-    private S fromStep;
-
-    // heuristic of destination
-    final double heuristic;
-
-    // gScore is the distance from the source
-    private double gScore;
-
-    // fScore = gScore + heuristic
-    private double fScore;
-
-    NodeData(S fromStep, double heuristic, double gScore) {
-      this.fromStep = fromStep;
-      this.heuristic = heuristic;
-      this.gScore = gScore;
-
-      this.fScore = gScore + heuristic;
-    }
-
-    //    public boolean equals(NodeData n) {}
-
-    @Override
-    public S getPredecessor() {
-      return fromStep;
-    }
-
-    public double getgScore() {
-      return gScore;
-    }
-
-    public void setgScore(double gScore) {
-      this.gScore = gScore;
-    }
-
-    public double getfScore() {
-      return fScore;
-    }
-
-    public void setfScore(double fScore) {
-      this.fScore = fScore;
-    }
-
-    public S getFromStep() {
-      return fromStep;
-    }
-
-    public void setFromStep(S fromStep) {
-      this.fromStep = fromStep;
-    }
-  }
 }
